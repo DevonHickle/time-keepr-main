a1a4ca921896274343df40a906526a639215e32f
Devon Hickle <devonhickle@Devons-MacBook-Pro.local>
2022-07-14 14:38:57 -0500

    adding express server and users API

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,12 @@ import { loadFonts } from './plugins/webfontloader'
 import Datepicker from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
 import BalmUI from 'balm-ui';
-import BalmUIPlus from 'balm-ui-plus';
 import 'balm-ui-css';
 
 loadFonts()
 
 createApp(App)
   .use(BalmUI)
-  .use(BalmUIPlus)
   .use(router)
   .use(vuetify)
   .component('Datepicker', Datepicker)
