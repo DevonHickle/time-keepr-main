e5b761ece9eca38a606299e47a8bd732e7eff867
Devon Hickle <devon.hickle@wnins.com>
2022-07-15 10:36:22 -0500

    updated notes on components

--- src/components/CurrentlyOffline.vue
+++ src/components/CurrentlyOffline.vue
@@ -2,15 +2,19 @@
   <div class="offline container">
     <div class="columns is-multiline">
       <div class="column is-one-quarter">
-        <p>This needs to get and return info from UsersInput and DatePicker</p>
+        <p>
+          This needs to get and return info from UsersInput and DatePicker and
+          match against the current date. If any users have selected times that
+          include the current date, they will show up here
+        </p>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-  export default {}
+export default {};
 </script>
 <style scoped>
-  /**/
+/**/
 </style>
--- src/components/UpcomingOffline.vue
+++ src/components/UpcomingOffline.vue
@@ -2,15 +2,20 @@
   <div class="upcoming container">
     <div class="columns is-multiline">
       <div class="column is-one-quarter">
-        <p>This needs to get and return info from UsersInput and DatePicker</p>
+        <p>
+          This needs to get and return info from UsersInput and DatePicker and
+          match against the next week. If any users have selected dates that are
+          within ahead week of the current date, they will show up here and
+          display the dates they are off
+        </p>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-  export default {}
+export default {};
 </script>
 <style scoped>
-  /**/
+/**/
 </style>
